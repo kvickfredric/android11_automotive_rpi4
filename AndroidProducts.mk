25ba30b68ea4dcf404980878957943e6bcdad1c2
Fredric_Kvick <fredric.kvick@gmail.com>
2020-12-01 14:22:10 +0100

    Working automotive device for android 11 on raspberry pi 4

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/rpi4car.mk
+
+COMMON_LUNCH_CHOICES := rpi4car-eng
---
+++ BoardConfig.mk
@@ -0,0 +1,74 @@
+#
+# Copyright 2020 Android-RPi Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := true
+TARGET_NO_RECOVERY := true
+
+ALLOW_MISSING_DEPENDENCIES=true
+
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := cortex-a72
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
+
+TARGET_USES_64_BIT_BINDER := true
+
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+BOARD_VNDK_VERSION := current
+
+TARGET_BOARD_PLATFORM := bcm2711
+
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648 # 1024M
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 134217728 # 128M
+
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_VENDORIMAGE_PARTITION_SIZE := 134217728 # 128M
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+MALLOC_SVELTE := true
+
+USE_XML_AUDIO_POLICY_CONF := 1
+
+BOARD_GPU_DRIVERS := v3d
+BOARD_USES_DRM_GRALLOC := true
+USE_OPENGL_RENDERER := true
+TARGET_USES_HWC2 := true
+TARGET_SCREEN_DENSITY := 150
+
+# Wifi
+BOARD_WLAN_DEVICE := bcmdhd
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/arpi/rpi4car/bluetooth
+BOARD_CUSTOM_BT_CONFIG := device/arpi/rpi4car/bluetooth/vnd_rpi4.txt
+
+BOARD_SEPOLICY_DIRS := \
+    device/arpi/rpi4car/sepolicy \
+
+DEVICE_MANIFEST_FILE := device/arpi/rpi4car/manifest.xml
